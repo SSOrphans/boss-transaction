04b701791e7cfb730905d8ed34f0383985d00a35
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-04-20 10:58:19 -0700

    Initial Push

---
+++ boss-controller/src/main/java/org/ssor/controller/BossControllerApplication.java
@@ -0,0 +1,15 @@
+package org.ssor.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossControllerApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(BossControllerApplication.class, args);
+  }
+
+}
---
+++ boss-controller/src/test/java/org/ssor/controller/BossControllerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.ssor.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossControllerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
