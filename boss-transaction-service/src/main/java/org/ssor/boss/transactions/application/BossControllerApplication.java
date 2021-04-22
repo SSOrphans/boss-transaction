d19623ee4d1923d2187bcd3121bcc142dc22c48b
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-04-22 16:44:51 -0700

    SSOR-161 Refactor: Spring Security Configuration Class

--- boss-transaction-service/src/main/java/org/ssor/boss/transactions/application/BossControllerApplication.java
+++ boss-transaction-service/src/main/java/org/ssor/boss/transactions/application/BossControllerApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.ssor.boss.transactions.repository.TransactionRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = "org.ssor.boss.transactions.*")
 @EntityScan(basePackages = "org.ssor.boss.entity")
 @EnableJpaRepositories(basePackageClasses = { TransactionRepository.class })
 @EnableSwagger2
-public class BossControllerApplication
+public class BossControllerApplication extends WebSecurityConfigurerAdapter
 {
 
   public static void main(String[] args)
