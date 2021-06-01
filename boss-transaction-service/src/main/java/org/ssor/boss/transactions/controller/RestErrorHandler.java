b78d0cc75ca888f5800c359f607601be407e22fb
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-05-31 23:19:47 -0700

    SSOR-227 SonarQube clean up

--- boss-transaction-service/src/main/java/org/ssor/boss/transactions/controller/RestErrorHandler.java
+++ boss-transaction-service/src/main/java/org/ssor/boss/transactions/controller/RestErrorHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.ssor.boss.core.exception.BadRouteException;
 import org.ssor.boss.core.exception.NoTransactionFoundException;
 import org.ssor.boss.transactions.transfer.ErrorMessage;
 
